eeb9842ffdab20a9657d895ffd1c764afccf08b8
blaipr <blaipr@hotmail.com>
2026-06-27 19:12:55 +0200

    Fix Installer::VERSION_TEXT references broken by version consolidation
    info.inc and openapi.php still used the removed VERSION_TEXT constant,
    causing a 500 on the configuration page. Replaced with
    AppInfoInterface::APP_VERSION.

--- public/api/openapi.php
+++ public/api/openapi.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use SP\Application\Install\Services\Installer;
+use SP\Domain\Core\AppInfoInterface;
 
 header('Content-Type: application/json; charset=utf-8');
 
 $spec = file_get_contents(__DIR__ . '/openapi.json');
-echo str_replace('@@VERSION@@', Installer::VERSION_TEXT, $spec);
+echo str_replace('@@VERSION@@', implode('.', array_slice(AppInfoInterface::APP_VERSION, 0, 2)), $spec);
