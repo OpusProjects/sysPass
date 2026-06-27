13ab38a96fa5a52cf0a402e2aaade8d18394db97
blaipr <blaipr@hotmail.com>
2026-06-27 13:50:02 +0200

    Add REST API endpoints for Public Links — CRUD + refresh

---
+++ src/Infrastructure/Adapter/In/Api/Controllers/PublicLink/SearchController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SP\Infrastructure\Adapter\In\Api\Controllers\PublicLink;
+
+use SP\Core\Events\Event;
+use SP\Domain\Api\Dtos\ApiResponse;
+use SP\Domain\Core\Acl\AclActionsInterface;
+use SP\Domain\Core\Dtos\ItemSearchDto;
+
+final class SearchController extends PublicLinkBase
+{
+    public function searchAction(): ApiResponse
+    {
+        $this->setupApi(AclActionsInterface::PUBLICLINK_SEARCH);
+
+        $itemSearchData = new ItemSearchDto(
+            $this->apiService->getParamString('text'),
+            0,
+            $this->apiService->getParamInt('count', false, self::SEARCH_COUNT_ITEMS)
+        );
+
+        $this->eventDispatcher->notify(new Event('search.publicLink', $this));
+
+        return ApiResponse::makeSuccess(
+            $this->publicLinkService->search($itemSearchData)->getDataAsArray()
+        );
+    }
+}
