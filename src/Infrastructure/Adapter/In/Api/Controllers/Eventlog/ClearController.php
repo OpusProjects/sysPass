d000655ebebc48ff80539166e7869b94d40c7395
Blai Peidro <blaipr@hotmail.com>
2026-06-27 13:45:04 +0200

    Add REST API endpoints for Event Log — search and clear (#223)

---
+++ src/Infrastructure/Adapter/In/Api/Controllers/Eventlog/ClearController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SP\Infrastructure\Adapter\In\Api\Controllers\Eventlog;
+
+use SP\Core\Events\Event;
+use SP\Core\Events\EventMessage;
+use SP\Domain\Api\Dtos\ApiResponse;
+use SP\Domain\Core\Acl\AclActionsInterface;
+
+use function SP\__;
+use function SP\__u;
+
+final class ClearController extends EventlogBase
+{
+    public function clearAction(): ApiResponse
+    {
+        $this->setupApi(AclActionsInterface::EVENTLOG_CLEAR);
+
+        $this->eventlogService->clear();
+
+        $this->eventDispatcher->notify(new Event('clear.eventlog',
+            $this,
+            EventMessage::build()->addDescription(__u('Event log cleared'))
+        ));
+
+        return ApiResponse::makeSuccess(null, __('Event log cleared'));
+    }
+}
---
+++ src/Infrastructure/Adapter/In/Api/Controllers/Eventlog/SearchController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SP\Infrastructure\Adapter\In\Api\Controllers\Eventlog;
+
+use SP\Core\Events\Event;
+use SP\Domain\Api\Dtos\ApiResponse;
+use SP\Domain\Core\Acl\AclActionsInterface;
+use SP\Domain\Core\Dtos\ItemSearchDto;
+
+final class SearchController extends EventlogBase
+{
+    public function searchAction(): ApiResponse
+    {
+        $this->setupApi(AclActionsInterface::EVENTLOG_SEARCH);
+
+        $itemSearchData = new ItemSearchDto(
+            $this->apiService->getParamString('text'),
+            0,
+            $this->apiService->getParamInt('count', false, self::SEARCH_COUNT_ITEMS)
+        );
+
+        $this->eventDispatcher->notify(new Event('search.eventlog', $this));
+
+        return ApiResponse::makeSuccess(
+            $this->eventlogService->search($itemSearchData)->getDataAsArray()
+        );
+    }
+}
